5f3614e7ef482ecbbb665e7b64be29e1f6ef27da
@Garay01 <garayalvarado14@gmail.com>
2020-09-07 09:32:34 -0600

    Quita espacios y agrega consultas para análisis de datos

--- src/DatabasesQueries.sql
+++ src/DatabasesQueries.sql
@@ -8,11 +8,11 @@ GO;
 -- Creacion de dimensiones (Se puede crear desde acá o en el componente de SSIS)
 create table DimCountry (
 id uniqueidentifier primary key,
-"name" nvarchar(255)
+"name" varchar(100)
 )
 create table DimTariff (
 id uniqueidentifier primary key,
-code nvarchar(10),
+code varchar(10),
 "description" nvarchar(255)
 )
 
@@ -36,4 +36,34 @@ drop table DimTariff;
 drop table Fact_imports;
 
 -- Consultas
-select * from Fact_imports
+--Calcular el total de un codigo alancerario de todos los paises con el paso de los años (1994-2016)
+select dt.code, sum(fi.value) as Total from Fact_imports fi
+join DimTariff dt on fi.id_tariff_cod = dt.id
+group by dt.code order by dt.code;
+
+--Calcular el total de un codigo alancerario y pais con el paso de los años (1994-2016)
+select dc.name, dt.code, sum(fi.value) as Total from Fact_imports fi
+join DimTariff dt on fi.id_tariff_cod = dt.id
+join DimCountry dc on fi.id_country = dc.id
+group by dt.code, dc.name order by dc.name, dt.code
+
+--Calcular el total gastado en un año especifo, por país
+select dc.name, fi.year, sum(fi.value) as Total from Fact_imports fi
+join DimCountry dc on fi.id_country = dc.id
+group by dc.name, fi.year order by dc.name, fi.year
+
+--Calcular el total gastado en un pais, incluyendo todos los codigos alancerarios con el paso de los años (1994-2016)
+select sum(fi.value) as Total, dc.name from Fact_imports fi
+join DimCountry dc on fi.id_country = dc.id
+group by dc.name;
+
+--Calcular el total por categoria en un año
+select fi.year, concat('Categoria ', substring(dt.code, 4, 1)) as Categoria, sum(fi.value) as Total from Fact_imports fi
+join DimTariff dt on fi.id_tariff_cod = dt.id
+group by concat('Categoria ', substring(dt.code, 4, 1)), fi.year order by fi.year, Categoria
+
+--Calcular el total por categoria de un pais a lo largo de los años (1994-2016)
+select dc.name, concat('Categoria ', substring(dt.code, 4, 1)) as Categoria, sum(fi.value) as Total from Fact_imports fi
+join DimTariff dt on fi.id_tariff_cod = dt.id
+join DimCountry dc on fi.id_country = dc.id
+group by concat('Categoria ', substring(dt.code, 4, 1)), dc.name order by dc.name, Categoria
